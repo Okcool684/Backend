58d17a52e622b1e9b7322af809aae542f2f70a48
Okcool684 <darshsinghvi05@gmail.com>
2025-06-19 10:57:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -119,4 +119,6 @@ def alerts():
     ])
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
